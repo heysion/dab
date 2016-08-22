a723e18f463a6fcb490485c8b5289bd2bb5ba68f
heysion@deepin <heysion@users.noreply.github.com>
2016-08-22 13:21:04 +0800

    update users  db access inteface  Changes to be committed: 	modified:   src/dabdb/da/users.py

--- src/dabdb/da/users.py
+++ src/dabdb/da/users.py
@@ -7,32 +7,22 @@
 @license: GPLv3
 '''
 
-# CREATE TABLE users (
-# 	id SERIAL NOT NULL PRIMARY KEY,
-# 	name VARCHAR(255) UNIQUE NOT NULL,
-# 	password VARCHAR(255),
-# 	status INTEGER NOT NULL,
-# 	usertype INTEGER NOT NULL,
-# 	krb_principal VARCHAR(255) UNIQUE
-# ) WITHOUT OIDS;
-
 from sqlalchemy import Column, ForeignKey, Integer, String
 
 from __init__ import Base,da_session,da_create_engine
 from __init__ import da_init_test as dainit
 
-class Users(Base):
-    __tablename__ = 'usersinfo'
+class User(Base):
+    __tablename__ = 'userinfo'
     id = Column(Integer, primary_key=True)
     name = Column(String(256), nullable=False)
     password = Column(String(256))
     status = Column(Integer)
     usertype = Column(Integer)
 
-
 def run_test():
     session = dainit(True)
-    new_obj = Users(name="test")
+    new_obj = User(name="test")
     session.add(new_obj)
     session.commit()
 
