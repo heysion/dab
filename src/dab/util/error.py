8844fead3145ed9b269ba27e334737549c62821e
heysion@deepin <heysion@users.noreply.github.com>
2016-08-26 01:34:21 +0800

    add error class  Changes to be committed: 	new file:   error.py

---
+++ src/dab/util/error.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2016-08-25
+@author: Heysion Yuan
+@copyright: 2016, Heysion Yuan <heysions@gmail.com>
+@license: GPLv3
+'''
+class ConfigException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
