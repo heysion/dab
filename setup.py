a0beda3660ee0536406f996cda7ad9bb41df4392
heysion@deepin <heysion@users.noreply.github.com>
2016-08-29 14:29:01 +0800

    add setup shell  Changes to be committed: 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+packages = find_packages("src",exclude=["*.tests.*", "tests.*", "tests"])
+print(packages)
+setup(
+    name="deepin-auto-build",
+    version="0.0.1",
+    author="Heysion Yuan",
+    package_dir={"dabutil":"src/dab/util","dabdaemon":"src/dabdaemon"},
+    packages=["dabutil","dabdaemon"],
+
+)
