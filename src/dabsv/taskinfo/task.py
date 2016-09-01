bc43cbf02fad0a8606e2a5eb693d66f0e1eaf809
heysion@deepin <heysion@users.noreply.github.com>
2016-09-01 18:00:42 +0800

    update taskinfo /task get method  Changes to be committed: 	modified:   src/dabsv/__init__.py 	modified:   src/dabsv/taskinfo/task.py

--- src/dabsv/taskinfo/task.py
+++ src/dabsv/taskinfo/task.py
@@ -6,13 +6,19 @@
 @copyright: 2016, Heysion Yuan <heysions@gmail.com>
 @license: GPLv3
 '''
-import tornado.web
+
+
+from dabdb.da.tasks import Task as daTask
+from dabsv import HandlerBase
+
 import pdb
 
-
-class TaskHandler(tornado.web.RequestHandler):
+class TaskHandler(HandlerBase):
     def get(self):
-        self.write("hello get")
+        self.http_buffer_loading()
+        if not (hasattr(self,"username") and hasattr(self,"password")) :
+            self.write(self.ret_404_msg("404 error"))
+        self.write("hello get%s"%(self.description))
         pass
     def post(self):
         self.write("hello post")
