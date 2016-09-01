1ff5489a8e8b5d57b930265defc1efcea1d0b91a
heysion@deepin <heysion@users.noreply.github.com>
2016-09-01 11:32:40 +0800

    fix setup.py  Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name="deepin-auto-build",
     version="0.0.1",
     author="Heysion Yuan",
-    package_dir={"dab":"src/dab/","dabdaemon":"src/dabdaemon","dabdb":"src/dabdb","dabsv":"src/dabsv/"},
-    packages=["dab","dabdaemon","dabdb","dabsv"],
-
+    packages = packages,
+    package_dir = {"":"src"},
 )
