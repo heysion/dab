f822e198fde1a152c976cfa49c3d0fd1eab4f7b7
heysion@deepin <heysion@users.noreply.github.com>
2016-09-02 18:41:14 +0800

    add test task get  Changes to be committed: 	new file:   src/test/test_task_dabsv.py

---
+++ src/test/test_task_dabsv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2016-09-01
+@author: Heysion Yuan
+@copyright: 2016, Heysion Yuan <heysions@gmail.com>
+@license: GPLv3
+'''
+import requests
+import json
+
+def test_task_get():
+    url = 'http://127.0.0.1:8080/task'
+    values = {'username': 'trusty',
+              'password': 'qwe123',
+              'channelname': 'dptest',
+              'hostname':'dptest'}
+
+    response = requests.get(url,data=json.dumps(values))
+    print response.content
+
+if __name__ == "__main__":
+    test_task_get()
