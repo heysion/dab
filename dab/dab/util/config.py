48bcb2661257e90b1d376d59d8d7a9404235381f
heysion@deepin <heysion@users.noreply.github.com>
2017-03-22 00:43:43 +0800

    rewrite daemonconfig  Changes to be committed: 	modified:   dab/util/config.py

--- dab/dab/util/config.py
+++ dab/dab/util/config.py
@@ -23,75 +23,86 @@ def str2bool(r):
 def str2int(r):
     return int(r)
 
-class DaemonConfig():
-    def __init__(self,config="/etc/dab/daemon.conf"):
-        if os.path.exists(config):
-            self.config = config
-        else:
-            raise ConfigError("not found config %s"%(config))
-        self.confp = ConfigParser()
-        self.confp.read(self.config)
-        if not self.confp.has_section("daemon"):
-            raise ConfigError("not found daemon section")
+class DaemonConfig(object):
+    def __new__(cls,*args,**kw):
+        if not hasattr(cls,"_instance"):
+            orig = super(DaemonConfig, cls)
+            cls._instace = orig.__new__(cls, *args, **kw)
+        return cls._instace
 
-    def update_options(self,options):
-        if not hasattr(self,"options"):
-            self.options = options
-        for key ,value in self.confp.items("daemon"):
-            if not getattr(self.options, key, None):
-                setattr(self.options, key, value)
-            pass
-        self.options.verbose = str2bool(self.options.verbose)
-        self.options.debug = str2bool(self.options.debug)
-
-        self.options.foreground = str2bool(self.options.foreground)
-        self.options.forcelock = str2bool(self.options.forcelock)
-
-        pass
-    @classmethod
-    def get_options(cls):
+    def update_options(self,dabargs):
         """process options from command line and config file"""
-        parser = OptionParser()
-        parser.add_option("-c", "--config", dest="config",
+        parser = argparse.ArgumentParser("[option]",description="daemon options from command line and config file")
+        parser.add_argument("-c", "--config", dest="config",
                           help="use alternate configuration file", metavar="FILE",
                           default="/etc/dab/daemon.conf")
 
-        parser.add_option("-p","--pid", dest="pidfile",
+        parser.add_argument("-p","--pid", dest="pidfile",
                           help="run pid file")
 
-        parser.add_option("-l","--log", dest="logfile",
+        parser.add_argument("-l","--log", dest="logfile",
                           help="daemon log file")
 
-        parser.add_option("-v", "--verbose", action="store_true", default=False,
+        parser.add_argument("-v", "--verbose", action="store_true", default=False,
                           help="show verbose output")
-        parser.add_option("-d", "--debug", action="store_true", default=False,
-                          help="show debug output")
+
+        parser.add_argument("-d", "--debug", dest="debug", action="store_true", 
+                            help="show debug output",default=False)
 
 
-        parser.add_option("--fg", dest="foreground",
+        parser.add_argument("--fg", dest="foreground",
                           action="store_true", default=False,
                           help="run in foreground")
-        parser.add_option("--forcelock", dest="forcelock",
+        parser.add_argument("--forcelock", dest="forcelock",
                           action="store_true", default=False,
                           help="force lock for exclusive session")
 
-        parser.add_option("--appname", help="specify appname")
-        parser.add_option("--username", help="specify user")
-        parser.add_option("--password", help="specify password")
+        parser.add_argument("--appname", help="specify appname")
+        parser.add_argument("--username", help="specify user")
+        parser.add_argument("--password", help="specify password")
 
-        parser.add_option("--maxjobs", type='int', help="Specify maxjobs")
-        parser.add_option("--sleeptime", type='int', help="Specify the polling interval")
-        parser.add_option("--server", help="url for server api")
-        parser.add_option("--topurl", help="Specify topurl")
+        parser.add_argument("--maxjobs", type=int, help="Specify maxjobs")
+        parser.add_argument("--sleeptime", type=int, help="Specify the polling interval")
+        parser.add_argument("--server", help="url for server api")
+        parser.add_argument("--topurl", help="Specify topurl")
 
-        parser.add_option("--topdir", help="Specify topdir")
-        parser.add_option("--workdir", help="Specify workdir")
-        parser.add_option("--arches", help="Specify arches")
+        parser.add_argument("--topdir", help="Specify topdir")
+        parser.add_argument("--workdir", help="Specify workdir")
+        parser.add_argument("--arches", help="Specify arches")
 
-        (options, args) = parser.parse_args()
-        return options
 
-    pass
+        options = parser.parse_args(args=dabargs)
+        # load config
+        if options.config:
+            if os.path.exists(options.config):
+                self.config = options.config
+            else:
+                raise ConfigError("not found config %s"%(config))
+            self.confp = ConfigParser()
+            self.confp.read(self.config)
+            if not self.confp.has_section("daemon"):
+                raise ConfigError("not found service section")
+            
+            for key ,value in self.confp.items("daemon"):
+                if not getattr(self, key, None):
+                    setattr(self, key, value)
+        #merge options to config
+        opt_list = vars(options)
+        for key in opt_list.keys():
+            if opt_list[key]:
+                setattr(self, key, opt_list[key])
+        if not isinstance(self.maxjobs, int):
+            self.maxjobs = int(self.maxjobs)
+        if not isinstance(self.sleeptime, int):
+            self.sleeptime = int(self.sleeptime)
+        if not isinstance(self.debug, bool):
+            self.debug = str2bool(self.debug)
+        if not isinstance(self.foreground, bool):
+            self.foreground = str2bool(self.foreground)
+        if not isinstance(self.forcelock, bool):
+            self.forcelock = str2bool(self.forcelock)
+        if not isinstance(self.verbose, bool):
+            self.verbose = str2bool(self.verbose)
 
 class DabsvConfig(object):
     def __new__(cls,*args,**kw):
@@ -102,7 +113,7 @@ class DabsvConfig(object):
 
     def update_options(self,dabargs):
         """process options from command line and config file"""
-        parser = argparse.ArgumentParser("[option]",description="process options from command line and config file")
+        parser = argparse.ArgumentParser("[option]",description="service options from command line and config file")
         parser.add_argument("-c", "--config", dest="config",
                           help="use alternate configuration file", metavar="FILE",
                           default="/etc/dab/dabsv.conf")
@@ -148,25 +159,10 @@ class DabsvConfig(object):
         pass
 
 if __name__ == "__main__":
-    opt = DabsvConfig()
+    opt = DaemonConfig()
     print sys.argv
     opt.update_options(sys.argv[1:])
     print(opt.__dict__)
-    # opt = DaemonConfig.get_options()
-    # print(opt.__dict__)
-    # test = DaemonConfig(opt.config)
-    # test.update_options(opt)
-    # print(test.options.__dict__)
-    # print(test.options.workdir)
-    # print(test.options.arches)
 
-# daemon_config = "cbsd.conf"
-# config = ConfigParser()
-# config.read(daemon_config)
-# for x in config.sections():
-#     pass
 
-# config.has_section("common")
-# print config.items("common")
-# print dict(config.items("cbsd"))
 
