0d92af9cf34fb1c94660d83d93d9e08a5e60f4d6
heysion@deepin <heysion@users.noreply.github.com>
2017-05-18 23:56:27 +0800

    merge dmd

---
+++ dab/settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@date: 2017-04-20
+@author: Heysion Yuan
+@copyright: 2017, Heysion Yuan <heysions@gmail.com>
+@license: GPLv3
+'''
+DMD_PATH="/opt/work"
+DMD_OUTPUT=DMD_PATH + "/output"
+DMD_UPLOAD=DMD_PATH + "/uploads"
+DMD_TOOLS_BIN=DMD_PATH + "/bin/mkdeepincd"
+DMD_CACHE=DMD_PATH + "/cache"
+DMD_TASK_PATH=DMD_PATH + "/tasks"
+DMD_REPO_PRE=DMD_PATH
+DMD_DEBIAN_CD_PATH=DMD_PATH + "/data/debian-cd"
