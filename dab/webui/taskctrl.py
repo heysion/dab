85a2e5ea101175a58783bf18928dffcd2342befa
heysion@deepin <heysion@users.noreply.github.com>
2017-06-27 11:28:22 +0800

    update task newtask api 	modified:   webui/taskctrl.py

--- dab/webui/taskctrl.py
+++ dab/webui/taskctrl.py
@@ -14,23 +14,17 @@ class TaskIndex(WebBase):
         dataset = self.get_task_top_all()
 
         self.render("taskindex.html",tasklist=dataset)
-        # task_items = [
-        #     {"id":1,"name":"deepin-auto-build","createtime":"2017","state":"success","resultinfo":"info"},
-        #     {"id":2,"name":"deepin-auto-build","createtime":"2017","state":"success","resultinfo":"info"}
-        # ]
-        # self.render("task.html", tasklist=task_items)
-        # pass
 
     def get_task_top_all(self):
-        dataset = Task.select(Task.id,Task.src_name,
-                              Task.create_time,Task.state,
-                              Task.owner_name)
+        dataset = Task.select(Task.id,Task.pkg_name,
+                              Task.target_name,Task.owner_name,
+                              Task.create_time,Task.state)
         datalist = []
         if dataset :
             for data in dataset:
-                datalist.append({"id":data.id,"name":data.name,
-                                 "ceatetime":data.createtime,"state":data.state,
-                                 "resultinfo":data.ower_name})
+                datalist.append({"id":data.id,"pkgname":data.pkg_name.name,
+                                 "targetname":data.target_name.name,"owername":data.owner_name.name,
+                                 "createtime":data.create_time,"state":data.state})
         return datalist
 
 class TaskInfo(WebBase):
@@ -43,24 +37,25 @@ class TaskNew(WebBase):
     
     def post(self):
         req_data = { k: self.get_argument(k) for k in self.request.arguments }
-        if not ("arches" in req_data.keys()):
+        if not ("arch" in req_data.keys()):
             self.render("404.html")
-        if not ("name" in req_data  and req_data["name"] is not None) :
+        if not ("pkgname" in req_data  and req_data["pkgname"] is not None) :
             self.render("404.html")
         self.save_new_task(req_data)
-        self.render("/taskindex")
+        self.redirect("/taskindex")
 
     def save_new_task(self,data):
-        new_task = Task.select(Task.name).where(Task.name==data["name"])
+        new_task = Task.select(Task.pkg_name).where(Task.pkg_name==data["pkgname"])
+        print(new_task)
         if not new_task :
-            new_task = Task.create(name=data["name"],
-                                       suite=data["suite"],
-                                       codename=data["codename"],
-                                       architectures=data["arches"],
-                                       workdir=data["workbase"],
-                                       description=data["description"])
+            new_task = Task.create(pkg_name=data["pkgname"],
+                                   target_name=data["target"],
+                                   arch=data["arch"],
+                                   ower_id=1,
+                                   ower_name="admin")
             new_task.save()
         else:
             return None
 
         return new_task
+
